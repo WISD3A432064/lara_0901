945e3b8b1324a553163ec86f4155eabba2efa173
nini <poikl15397@gmail.com>
2017-11-23 01:10:27 +0800

    3A432064 destroy內刪除資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -42,4 +42,9 @@ class AdminPostsController extends Controller
         return redirect()->route('admin.posts.index');
     }
 
+    public function destroy($id)
+    {
+    Post::destroy($id);
+    return redirect()->route('admin.posts.index');
+    }
 }
