61bb29c8075f52adb33eef064adf5e79279467dc
nini <poikl15397@gmail.com>
2017-11-22 23:29:24 +0800

    3A432064 使用 Model 查詢資料

---
+++ app/Http/Controllers/AdminPostsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class AdminPostsController extends Controller
+{
+    public function index()
+    {
+        $posts=Post::orderBy('created_at','DESC')->get();
+        $data=['posts'=>$posts];
+        return view('admin.posts.index',$data);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function edit($id)
+    {
+        $data = ['id' => $id];
+
+        return view('admin.posts.edit', $data);
+    }
+
+
+}
